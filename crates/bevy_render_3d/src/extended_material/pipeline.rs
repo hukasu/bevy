0701526aebc5156dca8710e10574eea7369a90f3
Lucas Farias <lucasfnf@gmail.com>
2025-03-26 18:49:09 -0300

    Pull `extended_material` to `bevy_render_3d`

---
+++ crates/bevy_render_3d/src/extended_material/pipeline.rs
@@ -0,0 +1,19 @@
+use bevy_asset::Handle;
+use bevy_render::render_resource::{BindGroupLayout, Shader};
+
+use crate::mesh_pipeline::pipeline::{MeshPipeline, MeshPipelineKey};
+
+use super::MaterialExtension;
+
+pub struct MaterialExtensionPipeline {
+    pub mesh_pipeline: MeshPipeline,
+    pub material_layout: BindGroupLayout,
+    pub vertex_shader: Option<Handle<Shader>>,
+    pub fragment_shader: Option<Handle<Shader>>,
+    pub bindless: bool,
+}
+
+pub struct MaterialExtensionKey<E: MaterialExtension> {
+    pub mesh_key: MeshPipelineKey,
+    pub bind_group_data: E::Data,
+}
